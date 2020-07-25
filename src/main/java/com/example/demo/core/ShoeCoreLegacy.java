0119e531b734ada5569600082578a65b0544bcab
Vincent Castelluci <vincent.castelluci@gmail.com>
2020-07-25 12:13:15 +0700

    feat: Use annotation instead of abstract class

--- src/main/java/com/example/demo/core/ShoeCoreLegacy.java
+++ src/main/java/com/example/demo/core/ShoeCoreLegacy.java
@@ -1,33 +1,23 @@
 package com.example.demo.core;
 
-import com.example.demo.dto.out.Shoe;
 import com.example.demo.dto.in.ShoeFilter;
 import com.example.demo.dto.in.ShoeFilter.Color;
+import com.example.demo.dto.out.Shoe;
 import com.example.demo.dto.out.Shoes;
 import java.math.BigInteger;
 import java.util.List;
-import org.springframework.stereotype.Component;
 
-@Component
+@Implementation(version = 1)
 public class ShoeCoreLegacy extends AbstractShoeCore {
-
-  @Override
-  protected BigInteger getVersion() {
-    return BigInteger.ONE;
-  }
 
   @Override
   public Shoes search(final ShoeFilter filter) {
     return Shoes.builder()
-                .shoes(
-                    List.of(
-                        Shoe.builder()
-                            .name("Legacy shoe")
-                            .color(Color.BLUE)
-                            .size(BigInteger.ONE)
-                            .build()
-                    )
-                )
+                .shoes(List.of(Shoe.builder()
+                                   .name("Legacy shoe")
+                                   .color(Color.BLUE)
+                                   .size(BigInteger.ONE)
+                                   .build()))
                 .build();
   }
 }
